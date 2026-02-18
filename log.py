be090b2ff17385d3a8a7aae37397d0d675848706
User <egorkathebestlordtop1gg@gmail.com>
2026-02-18 20:00:59 +0200

    commit

---
+++ log.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+import requests
+import threading
+import time
+import datetime
+import os
+import signal
+app = Flask(__name__)
+LOG_FILE = "logs.txt"
+RUNNING = True
+
+def heartbeat():
+    while RUNNING:
+        with open(LOG_FILE, "a", encoding="utf-8") as f:
+            f.write(f"{datetime.datetime.now()} HEARTBEAT OK\n")
+        time.sleep(60)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    price = None
+    coin = None
+    error = None
+
+
+    if request.method == "POST":
+        coin = request.form["coin"].upper()
+        url = f"https://api.coinbase.com/v2/exchange-rates?currency={coin}"
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            try:
+                price = data["data"]["rates"]["USD"]
+            except KeyError:
+                error = "Нема"
+        else:
+            error = "Error API"
+
+    logs = []
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE, "r", encoding="utf-8") as f:
+            logs = f.readlines()[-100:]
+
+    return render_template("index.html",price=price,coin=coin,error=error,logs=logs)
+@app.route("/kill")
+def kill():
+    os.kill(os.getpid(), signal.SIGTERM)
+    return "Killed"
+
+if __name__ == "__main__":
+    t = threading.Thread(target=heartbeat, daemon=True)
+    t.start()
+    app.run(port=2034, debug=False)
