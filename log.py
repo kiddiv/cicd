8b5537ab90474425bc188efd57121493043aacf0
User <egorkathebestlordtop1gg@gmail.com>
2026-03-23 20:45:58 +0200

    add prometheus metrics

--- log.py
+++ log.py
@@ -38,19 +38,14 @@ def index():
                 error = "Нема"
         else:
             error = "Error API"
-
+    elif "lol" in request.form:
+        smile = '😎'
     logs = []
     if os.path.exists(LOG_FILE):
         with open(LOG_FILE, "r", encoding="utf-8") as f:
             logs = f.readlines()[-100:]
 
-    return render_template("index.html",price=price,coin=coin,error=error,logs=logs)
-@app.route("/lolkek", methods=["GET", "POST"])
-def smile():
-    smile = None
-    if request.method == "POST":
-     smile = "😎"
-    return render_template("index.html",smile=smile)
+    return render_template("index.html",price=price,coin=coin,error=error,logs=logs,smile=smile)
 @app.route("/kill")
 def kill():
     os.kill(os.getpid(), signal.SIGTERM)
